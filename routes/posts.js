bafa1e976d9c413ae4a9fd22a60f0ac61715da81
Mindy D Tran <mindytran98@gmail.com>
2024-02-09 13:48:47 -0500

    get /

--- routes/posts.js
+++ routes/posts.js
@@ -5,8 +5,15 @@ import Post from "../models/posts.js";
 
 const router = new Router();
 
-router.get('/', async(req,res)=>{
-    res.send('testing');
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find({});
+    res.status(200).send(posts);
+  } catch (error) {
+    res
+      .status(404)
+      .json({ msg: "something went wrong", errormsg: error.message });
+  }
 });
 
 export default router;
