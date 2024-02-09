14757736987132ff946e65765ffcf6f35dd9b2c4
Mindy D Tran <mindytran98@gmail.com>
2024-02-09 10:44:31 -0500

    importing routers and checking if they're connected

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 
+// importing routes
+import usersRouter from "./routes/users.js";
+import postsRouter from "./routes/posts.js";
+import commentsRouter from "./routes/comments.js";
+
 const app = express();
 const PORT = process.env.PORT || 4000;
 
@@ -10,9 +15,13 @@ const PORT = process.env.PORT || 4000;
 app.use(cors()); // allows frontend to connect to backend
 app.use(morgan("dev")); // logger to know what requests you are calling
 app.use(express.json()); // for data in req.body
-app.use(express.urlencoded({extended: true})) // allow data in url
+app.use(express.urlencoded({ extended: true })); // allow data in url
 
-// routees
-app.get('/', (req,res)=> res.send("welcome to blog app api"));
+app.use("/api/users", usersRouter);
+app.use("/api/posts", postsRouter);
+app.use("/api/comments", commentsRouter);
 
-app.listen(PORT, ()=> console.log(`server is running on port ${PORT}`));
+// routes
+app.get("/", (req, res) => res.send("welcome to blog app api"));
+
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
