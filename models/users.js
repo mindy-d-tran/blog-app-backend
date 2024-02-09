311ee2c22c2df280a2edcda60570315d8ce70531
Mindy D Tran <mindytran98@gmail.com>
2024-02-09 09:37:25 -0500

    add fields

--- models/users.js
+++ models/users.js
@@ -1,5 +1,21 @@
 import mongoose from "mongoose";
 
-const usersSchema = new mongoose.Schema({});
+const usersSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 8,
+    maxLength: 30
+  },
+});
 
-export default mongoose.model('User', usersSchema);
+export default mongoose.model("User", usersSchema);
