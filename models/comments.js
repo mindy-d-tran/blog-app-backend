cdb5e562a2d9a28ff5d56be90d8499d83a44dfa6
Mindy D Tran <mindytran98@gmail.com>
2024-02-09 10:25:27 -0500

    add fields

--- models/comments.js
+++ models/comments.js
@@ -1,5 +1,35 @@
 import mongoose from "mongoose";
 
-const commentsSchema = new mongoose.Schema({});
+const user = new mongoose.Schema(
+  {
+    user_id: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+    },
+  },
+  { _id: false }
+);
 
-export default mongoose.model('Comment', commentsSchema);
+const commentsSchema = new mongoose.Schema(
+  {
+    user_id: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    post_id: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Post",
+      required: true,
+    },
+    comment_like: {
+      type: [user],
+    },
+    comment_content: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Comment", commentsSchema);
