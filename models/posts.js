a0800f94c9e63c5389de44d12d6cc93850088742
Mindy D Tran <mindytran98@gmail.com>
2024-02-09 10:00:21 -0500

    add timestamp

--- models/posts.js
+++ models/posts.js
@@ -21,33 +21,38 @@ const comment = new mongoose.Schema(
   { _id: false }
 );
 
-const postsSchema = new mongoose.Schema({
-  user_id: {
-    type: mongoose.Schema.ObjectId,
-    ref: "User",
-    required: true,
+const postsSchema = new mongoose.Schema(
+  {
+    user_id: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    post_likes: {
+      type: [user],
+    },
+    post_views: {
+      type: [user],
+    },
+    post_comments: {
+      type: [comment],
+    },
+    post_title: {
+      type: String,
+    },
+    post_content: {
+      type: String,
+    },
+    post_hastag: {
+      type: String,
+    },
+    post_img: {
+      type: String,
+    },
   },
-  post_likes: {
-    type: [user],
-  },
-  post_views: {
-    type: [user],
-  },
-  post_comments: {
-    type: [comment]
-  },
-  post_title: {
-    type: String,
-  },
-  post_content: {
-    type: String,
-  },
-  post_hastag: {
-    type: String,
-  },
-  post_img: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.model("Post", postsSchema);
