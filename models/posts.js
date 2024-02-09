085302e6bb5e4c2aa5cd0ac224706504887502b7
Mindy D Tran <mindytran98@gmail.com>
2024-02-09 09:57:38 -0500

    add type for fields

--- models/posts.js
+++ models/posts.js
@@ -1,14 +1,41 @@
 import mongoose from "mongoose";
 
+const user = new mongoose.Schema(
+  {
+    user_id: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { _id: false }
+);
+const comment = new mongoose.Schema(
+  {
+    comment_id: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Comment",
+      required: true,
+    },
+  },
+  { _id: false }
+);
+
 const postsSchema = new mongoose.Schema({
-  user_ID: {
+  user_id: {
     type: mongoose.Schema.ObjectId,
     ref: "User",
     required: true,
   },
-  post_likes: {},
-  post_views: {},
-  post_comments: {},
+  post_likes: {
+    type: [user],
+  },
+  post_views: {
+    type: [user],
+  },
+  post_comments: {
+    type: [comment]
+  },
   post_title: {
     type: String,
   },
