7537d4a50c734009d7330e46fe32d1eeadf7280b
Mindy D Tran <mindytran98@gmail.com>
2024-02-09 18:27:50 -0500

    create post / and delete /:id

--- routes/comments.js
+++ routes/comments.js
@@ -21,7 +21,7 @@ router.get("/", async (req, res) => {
 // GET 1 comment
 router.get("/:id", async (req, res) => {
   try {
-    const comment = Comment.findById(req.params.id);
+    const comment = await Comment.findById(req.params.id);
     res.send(comment);
   } catch (error) {
     res
@@ -31,19 +31,41 @@ router.get("/:id", async (req, res) => {
 });
 
 // POST (create) new comment
-router.post('/', async(req,res)=>{
-    try {
-        const newComment = await Comment.create(req.body);
-        const post = await Post.findById(req.body.post_id);
-        if(post) {
-            post.post_comments.push({comment_id: newComment._id});
-            await post.save();
-        }
-        res.status(201).send(newComment);
-    } catch (error) {
-        res
+router.post("/", async (req, res) => {
+  try {
+    const newComment = await Comment.create(req.body);
+    const post = await Post.findById(req.body.post_id);
+    if (post) {
+      post.post_comments.push({ comment_id: newComment._id });
+      await post.save();
+    }
+    res.status(201).send(newComment);
+  } catch (error) {
+    res
       .status(404)
       .json({ msg: "something went wrong", errormsg: error.message });
-    }
-})
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    // find comment to find post_id
+    const comment = await Comment.findById(req.params.id);
+
+    //find post 
+    const post = await Post.findById(comment.post_id);
+    // delete the comment from the array
+    post.post_comments.pull({comment_id: req.params.id});
+    await post.save();
+
+    // delete comment from database
+    const deleteComment = await Comment.findByIdAndDelete(req.params.id);
+    res.json({ msg: "comment deleted", deleteComment });
+
+  } catch (error) {
+    res
+      .status(404)
+      .json({ msg: "something went wrong", errormsg: error.message });
+  }
+});
 export default router;
