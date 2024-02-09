8803f7c8e344853b5149f7ed2e4fac636dd34606
Mindy D Tran <mindytran98@gmail.com>
2024-02-09 09:48:44 -0500

    add fields

--- models/posts.js
+++ models/posts.js
@@ -1,5 +1,26 @@
 import mongoose from "mongoose";
 
-const postsSchema = new mongoose.Schema({});
+const postsSchema = new mongoose.Schema({
+  user_ID: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  post_likes: {},
+  post_views: {},
+  post_comments: {},
+  post_title: {
+    type: String,
+  },
+  post_content: {
+    type: String,
+  },
+  post_hastag: {
+    type: String,
+  },
+  post_img: {
+    type: String,
+  },
+});
 
-export default mongoose.model('Post', postsSchema);
+export default mongoose.model("Post", postsSchema);
