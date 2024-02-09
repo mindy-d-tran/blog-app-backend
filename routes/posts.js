b1fc84f3b5275b1790aa3dcdb279d6df073a33e7
Mindy D Tran <mindytran98@gmail.com>
2024-02-09 14:35:22 -0500

    add if statement to get /:id to increase the post view if get request is successful and create post /

--- routes/posts.js
+++ routes/posts.js
@@ -21,7 +21,24 @@ router.get("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
+    // if get post then increase the view by one
+    if (post) {
+      post.post_views++;
+      await post.save();
+    }
     res.status(200).send(post);
+  } catch (error) {
+    res
+      .status(404)
+      .json({ msg: "something went wrong", errormsg: error.message });
+  }
+});
+
+// POST (create) a new post
+router.post("/", async (req, res) => {
+  try {
+    const newPost = await Post.create(req.body);
+    res.status(201).send(newPost);
   } catch (error) {
     res
       .status(404)
